88e85b183719194c9147a507fa1ac152a7041562
Prejith <prajithpremg@gmail.com>
2018-02-09 15:15:16 +0530

    Add intro screens

---
+++ app/src/main/java/com/example/hbg/appster/MainIntro.java
@@ -0,0 +1,14 @@
+package com.example.hbg.appster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+
+public class MainIntro extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
