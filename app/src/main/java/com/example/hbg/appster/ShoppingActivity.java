afddaa4432e00afd10003159880fd502ec958786
Prabhashankar <shankar75031@gmail.com>
2018-02-09 15:01:26 +0530

    shopping activity and gradle change

--- app/src/main/java/com/example/hbg/appster/ShoppingActivity.java
+++ app/src/main/java/com/example/hbg/appster/ShoppingActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class ShoppingActivity extends AppCompatActivity {
 
     ListView listView;
@@ -11,6 +13,11 @@ public class ShoppingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home);
-        listView = (ListView) find
+        listView = (ListView) findViewById(R.id.list_customer_home);
+
+        ArrayList<Item> itemArrayList= new ArrayList<Item>();
+        itemArrayList.add(new Item("Hello", "200"));
+        ItemsAdapter itemsAdapter = new ItemsAdapter(this, itemArrayList);
+        listView.setAdapter(itemsAdapter);
     }
 }
