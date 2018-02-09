20e3792000aa178e169340e3cf0f5d6d96b12815
Prabhashankar <shankar75031@gmail.com>
2018-02-09 14:05:37 +0530

    added list

--- app/src/main/java/com/example/hbg/appster/Item.java
+++ app/src/main/java/com/example/hbg/appster/Item.java
@@ -1,3 +1,5 @@
+package com.example.hbg.appster;
+
 /**
  * Created by hbg on 2/9/2018.
  */
@@ -5,5 +7,28 @@
 public class Item {
     String name;
     String price;
-    
+
+    public Item() {
+    }
+
+    public Item(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
--- app/src/main/java/com/example/hbg/appster/MainActivity.java
+++ app/src/main/java/com/example/hbg/appster/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.hbg.appster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText uname, pass;
+    Button login;
+    String namestr,passwordstr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        uname=(EditText)findViewById(R.id.uname);
+        pass = (EditText) findViewById(R.id.pass);
+        login = (Button) findViewById(R.id.loginbutton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                namestr = uname.getText().toString();
+                passwordstr = pass.getText().toString();
+                if(namestr.equals("admin")){
+                    startActivity(new Intent(MainActivity.this, AddItemActivity.class));
+                }
+                else{
+                    startActivity(new Intent(MainActivity.this, ShoppingActivity.class));
+                }
+            }
+        });
     }
 }
